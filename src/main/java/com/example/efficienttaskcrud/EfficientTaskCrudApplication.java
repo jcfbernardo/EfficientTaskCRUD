19577fb7c9dcda9f9cc19862bdcab0436a283885
João Carlos Fernandes Bernardo <joaoc.bernardo12@gmail.com>
2023-08-22 21:33:07 -0300

    first commit

---
+++ src/main/java/com/example/efficienttaskcrud/EfficientTaskCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.efficienttaskcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficientTaskCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfficientTaskCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/efficienttaskcrud/EfficientTaskCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efficienttaskcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientTaskCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
